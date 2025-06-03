0d323643e0e0b9a54c1120cc1646637c153f5cf1
dariiatsvlk <dashacv06@gmail.com>
2025-06-03 21:41:14 +0300

    Constants.cs

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,23 +7,23 @@ using System.Threading.Tasks;
 namespace CurrencyExchanger.Utils
 {
     public static class Constants
-{
-    public static string TelegramBotToken =>
-        Environment.GetEnvironmentVariable("TelegramBotToken") ?? throw new Exception("Missing TelegramBotToken");
+    {
+        public static string TelegramBotToken =>
+            Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN") ?? throw new Exception("Missing TELEGRAM_BOT_TOKEN");
 
-    public static string ExchangeRatesAppId =>
-        Environment.GetEnvironmentVariable("ExchangeRatesAppId") ?? throw new Exception("Missing ExchangeRatesAppId");
+        public static string ExchangeRatesAppId =>
+            Environment.GetEnvironmentVariable("EXCHANGE_RATES_APP_ID") ?? throw new Exception("Missing EXCHANGE_RATES_APP_ID");
 
-    public static string ExchangeRatesBaseUrl =>
-        Environment.GetEnvironmentVariable("ExchangeRatesBaseUrl") ?? "https://openexchangerates.org/api/";
+        public static string ExchangeRatesBaseUrl =>
+            Environment.GetEnvironmentVariable("EXCHANGE_RATES_BASE_URL") ?? "https://openexchangerates.org/api/";
 
-    public static string DefaultBaseCurrency => "USD";
+        public static string DefaultBaseCurrency =>
+            Environment.GetEnvironmentVariable("DEFAULT_BASE_CURRENCY") ?? "USD";
 
-    public static string ApiBaseUrl =>
-        Environment.GetEnvironmentVariable("ApiBaseUrl") ?? "https://localhost:5001";
+        public static string ApiBaseUrl =>
+            Environment.GetEnvironmentVariable("API_BASE_URL") ?? "https://localhost:5001";
 
-    public static string DbConnectionString =>
-        Environment.GetEnvironmentVariable("DbConnectionString") ?? throw new Exception("Missing DbConnectionString");
-}
-
+        public static string DbConnectionString =>
+            Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ?? throw new Exception("Missing DB_CONNECTION_STRING");
+    }
 }
